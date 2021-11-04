21f8bd27847281ef5932a7439c29492b20a3e3e4
DanielleEmely <danielleemily92014@gmail.com>
2021-11-04 19:44:29 -0300

    Alterando os comentarios em models

--- playfavs/appPlaylist/models.py
+++ playfavs/appPlaylist/models.py
@@ -8,13 +8,9 @@ class Artista(models.Model):
         return self.nome_do_artista
 
 class Playlist(models.Model):
+    # relacionamento (1:n), um usuario possui uma ou várias playlists
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
     nome_playlist = models.CharField(max_length=200)
-    # na playlist, com o ForeignKey de model, temos um relacionmaneto (1:n), onde há um referência para musica
-    # portanto, caso eu queira acessar os dados daquela musica, posso pegá-los pela chave FK
-    # com isso, são acessados, titulo e artista, e por consequência do relacionamento da
-    # musica com seu artista, pode-se chegar até os dados do artista de uma música também.
-    # musicas = models.ForeignKey(Musica, on_delete=models.CASCADE, default=1)
     criado = models.DateTimeField(auto_now_add=True)
     atualizado = models.DateTimeField(auto_now=True)
 
@@ -25,6 +21,10 @@ class Musica(models.Model):
     titulo = models.CharField(max_length=200)
     #toda musica está vinculada a um artista (com restrição de integridade referencial de exluscao)
     artista_da_musica = models.ForeignKey(Artista, on_delete=models.CASCADE)
+    # Com a ForeignKey no model Musicas, temos um relacionmaneto (1:n), onde há um referência de n musicas para 1 playlist
+    # portanto, caso eu queira acessar os dados das musicas de uma determinada plalist, posso pegá-las pela chave FK
+    # com isso, são acessados, titulo e artista, e por consequência do relacionamento da
+    # musica com seu artista, pode-se chegar até os dados do artista de uma música também.
     playlist_de_origem = models.ForeignKey(Playlist, on_delete=models.CASCADE)
     def __str__(self):
         return self.titulo
